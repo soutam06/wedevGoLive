41fb06a47f0d5d581c32622b1de37a5b57480e8f
soutam <rajbharsoutam@gmail.com>
2024-12-19 14:13:02 +0530

    minor changes added

--- scripy.js
+++ scripy.js
@@ -1,32 +1,6 @@
-// single element selector
-// console.log(document.getElementById('my-form'));
-// console.log(document.querySelector('h1'));
-
-// Multiple element selector
-// console.log(document.querySelectorAll('.item'))
-
-// const ul = document.querySelector('.items');
-
-// ul.remove();
-// ul.lastElementChild.remove()
-// ul.firstElementChild.textContent = 'Hello'
-// ul.children[1].innerText = 'soutam';
-// ul.lastElementChild.innerHTML = '<h1> Hello</h1>'
-
-// const btn = document.querySelector('.btn');
-
-// btn.style.background = 'red';
-
-// btn.addEventListener('click',function(e){
-//     e.preventDefault();  
-//     document.querySelector('#my-form').style.background = '#ccc';
-    // document.querySelector('body').classList.add('bg-dark');
-    // document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
-// });
-
 const myForm = document.querySelector('#my-form');
-const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
+const first_name = document.querySelector('#first_name');
+const last_name = document.querySelector('#last_name');
 const msg = document.querySelector('.msg');
 const usersList = document.querySelector('#users');
 
@@ -34,8 +8,8 @@ myForm.addEventListener('submit',onsubmit);
 
 function onsubmit(e) {
     e.preventDefault();
-    // console.log(nameInput.value)
-    if(nameInput.value === '' || emailInput.value === ''){
+
+    if(first_name.value === '' || last_name.value === ''){
         // alert('Please enter Fields');
         msg.classList.add('error');
         msg.innerHTML = 'Please Enter a Fields';
@@ -43,7 +17,7 @@ function onsubmit(e) {
         setTimeout(() => msg.remove(),3000);
     } else {
         const li = document.createElement('li');
-        li.appendChild(document.createTextNode(nameInput.value+ " : "+emailInput.value));
+        li.appendChild(document.createTextNode("Welcome to my Website "+first_name.value+ " "+last_name.value));
 
         usersList.appendChild(li);
 
